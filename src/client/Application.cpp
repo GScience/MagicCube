b91dab26136facd2e79f8d66191bb2a2747170ed
GM2000 <GM2000@outlook.com>
2018-02-22 20:01:41 +0800

    Fix test

--- src/client/Application.cpp
+++ src/client/Application.cpp
@@ -98,24 +98,3 @@ int Application::run()
 
 	return 0;
 }
-
-int main(const int argc, char* argv[])
-{
-	std::vector<std::string> args(argc);
-
-	for (auto i = 0; i < argc; i++)
-		args[i] = argv[i];
-
-	try
-	{
-		return Application(args).run();
-	}
-	catch(std::exception& e)
-	{
-		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Program throw an error: %s", e.what());
-		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "ERROR", e.what(), nullptr);
-
-		assert(1);
-		return -1;
-	}
-}
